4cfa3c1c076459c800603413a913f56873b85c29
CCB Admin <admin@ccb.edu.ph>
2026-05-29 17:07:24 +0800

    Improvised: UI/UX for Admin Dashboard v2

--- ccbwebsite/settings.py
+++ ccbwebsite/settings.py
@@ -99,7 +99,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-WHITENOISE_USE_FINDERS = DEBUG
+WHITENOISE_USE_FINDERS = True
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
--- ccbwebsite/urls.py
+++ ccbwebsite/urls.py
@@ -1,9 +1,19 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.admin import __file__ as admin_file
 from django.urls import include, path
+from django.views.static import serve
+from pathlib import Path
+
+admin_static_root = Path(admin_file).resolve().parent / "static" / "admin"
 
 urlpatterns = [
+    path(
+        "static/admin/<path:path>",
+        serve,
+        {"document_root": admin_static_root},
+    ),
     path(settings.ADMIN_URL, admin.site.urls),
     path("", include("website.urls")),
 ]
