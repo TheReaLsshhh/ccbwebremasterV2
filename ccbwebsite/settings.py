7ccfa48a02c8f4dfaedd2a1201a16a49887c512c
CCB Admin <admin@ccb.edu.ph>
2026-05-29 14:12:59 +0800

    Improvised: UI/UX for all the pages version 2: added fix on settings.py

--- ccbwebsite/settings.py
+++ ccbwebsite/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-WHITENOISE_USE_FINDERS = True
+WHITENOISE_USE_FINDERS = DEBUG
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
@@ -108,7 +108,7 @@ STORAGES = {
         ),
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
