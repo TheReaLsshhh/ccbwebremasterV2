38c992c41921cd9fab246ad7d59e4ab47bb90881
CCB Admin <admin@ccb.edu.ph>
2026-06-08 15:31:43 +0800

    Frontend: Contact Form Implementation v4

--- ccbwebsite/settings.py
+++ ccbwebsite/settings.py
@@ -238,6 +238,7 @@ EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = env_bool("EMAIL_USE_TLS", True)
+EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT", "10"))
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "City College of Bayawan <no-reply@citycollegeofbayawan.edu.ph>")
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 CONTACT_INQUIRY_RECIPIENT = os.getenv("CONTACT_INQUIRY_RECIPIENT", "citycollegeofbayawan@gmail.com")
